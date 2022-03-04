0e5a80f54f157822936e4cd539a1169fc36a32cd
Vaibhav360-web <patilvaibhu1656@gmail.com>
2022-03-04 14:15:12 +0530

    MODIFIED: Wage Calculation

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -3,29 +3,20 @@ import java.util.Scanner;
 
 public class EmpWageBuilder{
     public static void main(String[] args) {
+        final int F=20;
+        final int W=8;
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int k=r.nextInt()%2;
-        if(k%2==0){
-            System.out.println("Employee Is Present");
-            Random v = new Random();
-            int n=v.nextInt()%2;
-            if(n%2==0)
-            {
-                int Wages=100;
-                System.out.println("Salary="+Wages);
-
-            }
-            else{
-                int Wages = 40;
-                System.out.println("Salary="+Wages);
-
-            }
-        }
-        else
-        {
-            System.out.println("Employee Is Absent");
+        int k = r.nextInt() % 2;
+        switch(k){
+            case 0:
+                int t=W*F;
+                System.out.println("Total Wage Is="+t+"$");
+                break;
+            default:
+                System.out.println("The Total Wage is 56$");
+                break;
         }
 
     }
