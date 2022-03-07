5ee35a6038570469a0039248225dcbb74ed3fa1c
Vaibhav360-web <patilvaibhu1656@gmail.com>
2022-03-07 13:07:27 +0530

    MODIFIED: Wages for Temporary and Permanent emp

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -5,17 +5,33 @@ public class EmpWageBuilder{
     public static void main(String[] args) {
         final int F=20;
         final int W=8;
+        final int P=4;
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
 
         int k = r.nextInt() % 2;
         switch(k){
+            case 1:
+
+                System.out.println("Employee is Present");
+                Random v = new Random();
+                int d= v.nextInt()%2;
+                switch (d){
+                    case 1:
+                        int t=W*F;
+                        System.out.println("The Employee is Permanent & Salary is="+t+"$");
+                        break;
+                    default:
+                        t=F*P;
+                        System.out.println("The Employee is Temporary & Salary is="+t+"$");
+                        break;
+                }
+
             case 0:
-                int t=W*F;
-                System.out.println("Total Wage Is="+t+"$");
+                System.out.println("Employee is Absent");
                 break;
             default:
-                System.out.println("The Total Wage is 56$");
+                System.out.println("LoL");
                 break;
         }
 
