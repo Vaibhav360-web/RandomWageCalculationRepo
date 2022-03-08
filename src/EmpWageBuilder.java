44be72575dc3cf7c2b4b0396c131a4c829f691a5
Vaibhav360-web <patilvaibhu1656@gmail.com>
2022-03-08 12:32:09 +0530

    MODIFIED:Emp Wages Program using For Loop

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -3,38 +3,38 @@ import java.util.Scanner;
 
 public class EmpWageBuilder{
     public static void main(String[] args) {
-        final int F=20;
-        final int W=8;
-        final int P=4;
+        final int Emp_Daily_Wages=20;
+        final int Emp_Per_Hours=8;
+        final int Emp_Tem_Hours=4;
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int k = r.nextInt() % 2;
-        switch(k){
-            case 1:
+        for(int i=0;i<=20;i++) {
+            int k = r.nextInt() % 2;
+            switch (k) {
+                case 1:
+                    System.out.println("Employee is Present");
+                    Random v = new Random();
+                    int d = v.nextInt() % 2;
+                    switch (d) {
+                        case 1:
+                            int Total = Emp_Per_Hours * Emp_Daily_Wages;
+                            System.out.println("The Employee is Permanent & Salary is=" + Total + "$");
+                            break;
+                        default:
+                            Total = Emp_Daily_Wages * Emp_Tem_Hours;
+                            System.out.println("The Employee is Temporary & Salary is=" + Total + "$");
+                            break;
+                    }
 
-                System.out.println("Employee is Present");
-                Random v = new Random();
-                int d= v.nextInt()%2;
-                switch (d){
-                    case 1:
-                        int t=W*F;
-                        System.out.println("The Employee is Permanent & Salary is="+t+"$");
-                        break;
-                    default:
-                        t=F*P;
-                        System.out.println("The Employee is Temporary & Salary is="+t+"$");
-                        break;
-                }
-
-            case 0:
-                System.out.println("Employee is Absent");
-                break;
-            default:
-                System.out.println("LoL");
-                break;
+                case 0:
+                    System.out.println("Employee is Absent");
+                    break;
+                default:
+                    System.out.println("LoL");
+                    break;
+            }
         }
-
     }
 }
 
