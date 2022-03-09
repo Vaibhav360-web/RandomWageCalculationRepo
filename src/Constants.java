72436efae9dbeff63d22bd26dbe918daf0abeeb9
Vaibhav360-web <patilvaibhu1656@gmail.com>
2022-03-09 22:42:24 +0530

    MODIFIED:Wage Calculation with Hours and Days with Constant Class

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    final static int EMP_DAILY_WAGES = 20;
+    final static int EMP_PER_HOURS = 20;
+    final static int EMP_TEM_HOURS = 15;
+    final static int Emp_Total_Days = 30;
+    final static int Emp_Present_Hours = 100;
+    final static int Emp_Present_Days = 20;
+}
--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -2,14 +2,12 @@ import java.util.Random;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        final int EMP_DAILY_WAGES = 20;
-        final int EMP_PER_HOURS = 20;
-        final int EMP_TEM_HOURS = 15;
-        int Emp_Total_Hours=0;
-        int Emp_Total_Days=0;
+        int Emp_Total_Hours = 0;
+        int Emp_Total_Days = 0;
         Random random = new Random();
 
-        for (int i = 0; i <= 30; i++) {
+        for (int i = 1; i <= Constants.Emp_Total_Days; i++) {
+            System.out.println("Day:" + i);
             int Binary_Number_Random = random.nextInt() % 2;
             switch (Binary_Number_Random) {
                 case 1:
@@ -18,33 +16,32 @@ public class EmpWageBuilder {
                     int Binary_Number_Random1 = random1.nextInt() % 2;
                     switch (Binary_Number_Random1) {
                         case 1:
-                            int Total = EMP_PER_HOURS * EMP_DAILY_WAGES;
+                            int Total = Constants.EMP_PER_HOURS * Constants.EMP_DAILY_WAGES;
                             System.out.println("->The Employee is Permanent & Salary is=" + Total + "$");
-                            Emp_Total_Hours=Emp_Total_Hours+EMP_PER_HOURS;
-                            Emp_Total_Days=Emp_Total_Days+1;
+                            Emp_Total_Hours = Emp_Total_Hours + Constants.EMP_PER_HOURS;
+                            Emp_Total_Days = Emp_Total_Days + 1;
                             break;
                         default:
-                            Total = EMP_DAILY_WAGES * EMP_TEM_HOURS;
+                            Total = Constants.EMP_DAILY_WAGES * Constants.EMP_TEM_HOURS;
                             System.out.println("->The Employee is Temporary & Salary is=" + Total + "$");
-                            Emp_Total_Hours=Emp_Total_Hours+EMP_TEM_HOURS;
-                            Emp_Total_Days=Emp_Total_Days+1;
+                            Emp_Total_Hours = Emp_Total_Hours + Constants.EMP_TEM_HOURS;
+                            Emp_Total_Days = Emp_Total_Days + 1;
                             break;
                     }
 
                 case 0:
+                case -1:
                     System.out.println("*Employee is Absent");
                     break;
-                default:
-                    System.out.println("*LoL");
-                    break;
+
             }
-            System.out.println("The Hours Are="+Emp_Total_Hours);
-            if(Emp_Total_Hours>=100 || Emp_Total_Days==20) {
+            System.out.println("The Hours Are=" + Emp_Total_Hours);
+            if (Emp_Total_Hours >= Constants.Emp_Present_Hours || Emp_Total_Days == Constants.Emp_Present_Days) {
                 i = 31;
-                if(Emp_Total_Hours>=100){
+                if (Emp_Total_Hours >= Constants.Emp_Present_Hours) {
                     System.out.println("The Employee Has Completed his 100 Hrs of Month");
                 }
-                if (Emp_Total_Days==20){
+                if (Emp_Total_Days == Constants.Emp_Total_Days) {
                     System.out.println("The Employee has Completed his 20 Days of Month");
                 }
 
